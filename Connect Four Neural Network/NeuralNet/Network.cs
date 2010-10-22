e7ac77f4dd71cf8ec79d6997f648e1eba00b9e85
jason@long.name <jason@long.name@d487a409-c6bb-0f7a-8971-a662c59b5e07>
2010-10-22 21:46:11 +0000

    document constructor for Network class

--- Connect Four Neural Network/NeuralNet/Network.cs
+++ Connect Four Neural Network/NeuralNet/Network.cs
@@ -19,7 +19,15 @@ namespace NeuralNet
 		public Termination Termination;
 
 		private Network() { }
+
+		/// Create a new (randomized) 3-layer neural network.
+		///  inputs: the number of input nodes
+		///  hiddens: the number of hidden nodes (middle layer)
+		///  outputs: the number of output nodes
+		///  termination:
+		///  parameters: may be null
 		public Network(int inputs, int hiddens, int outputs, Termination termination, NetworkParameters parameters = null) : this (inputs, new List<int>(){hiddens}, outputs, termination, parameters) {}
+
 		public Network(int inputs, List<int> hiddens, int outputs, Termination termination, NetworkParameters parameters = null, int constants = 1)
 		{
 			if (parameters == null)
