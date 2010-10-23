ce7e90a1affdf16b6583b1d793bfe196101f502b
jason@long.name <jason@long.name@d487a409-c6bb-0f7a-8971-a662c59b5e07>
2010-10-23 02:18:52 +0000

    Bot: specify which side the bot is playing for in constructor

--- trunk/Connect Four Neural Network/NeuralNet/Bot.cs
+++ trunk/Connect Four Neural Network/NeuralNet/Bot.cs
@@ -8,29 +8,34 @@ namespace NeuralNet
 	class Bot
 	{
 		Network network;
+		int my_color;
+
 		const int NUM_INPUT_NODES = ConnectFourBoard.NUM_ROWS * ConnectFourBoard.NUM_COLUMNS;
 		const int NUM_HIDDEN_NODES = 100;
 
-		public Bot()
+		public Bot(int _my_color)
 		{
 			// construct a neural network with 42 input nodes,
 			// one output node, and some number of hidden nodes.
 			network = new Network(NUM_INPUT_NODES,
 				NUM_HIDDEN_NODES, 1, null);
+			my_color = _my_color;
 		}
 
 		double EvaluateBoard(ConnectFourBoard board)
 		{
-			double [] cells = new double[ConnectFourBoard.NUM_ROWS * ConnectFourBoard.NUM_COLUMNS];
+			double [] cells = new double[NUM_INPUT_NODES];
 			for (int y = 0; y < ConnectFourBoard.NUM_ROWS; y++)
 			{
 				for (int x = 0; x < ConnectFourBoard.NUM_COLUMNS; x++)
 				{
-					cells[y * ConnectFourBoard.NUM_COLUMNS + x] = board.getCell(y,x);
+					int tmp = board.getCell(y,x);
+					cells[y * ConnectFourBoard.NUM_COLUMNS + x]
+						= tmp == 0 ? 0.0 :
+						tmp == my_color ? 1.0 : -1.0;
 				}
 			}
 			List<double> inputValues = new List<double>(cells);
-			//inputValues.AddRange(cells);
 			List<double> outputValues = network.Apply(inputValues);
 			return outputValues[0];
 		}
