4858a0242ab40d9c78e52a84bd60420625650192
verythaiguy@gmail.com <verythaiguy@gmail.com@d487a409-c6bb-0f7a-8971-a662c59b5e07>
2010-10-29 06:00:52 +0000

    Finished a lot of the Connect 4 Gui.  Still need to implement Human Vs. Computer, which should be easy.  I just ran out of time after working 15 straight hours lol.

--- trunk/Connect Four Neural Network/ConnectFour/Simulator.cs
+++ trunk/Connect Four Neural Network/ConnectFour/Simulator.cs
@@ -8,70 +8,62 @@ namespace ConnectFour
 {
     class Simulator
     {
-        Log Logger = new Log();
         int TotalGames, JasonWon, AllenWon, Ties, Turns, TotalTurns;
-        bool LogProgress = false;
+        GameViewer Viewer { get; set; }
+        Log Log = new Log();
 
-        public Simulator()
-        {
-            Logger.btnPlay.Click += new System.Windows.RoutedEventHandler(btnPlay_Click);
-        }
 
-        void btnPlay_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            Play(logProgress:true);
-        }
-            
         /// <summary>
         /// Simulate a game until completion.
         /// </summary>
         /// <param name="board">Starting board that the bots will play on.  This need not be empty!</param>
         /// <param name="network">Neural network that provides the AI for gameplay.</param>
         /// <returns>Trace of game sequence, each board state stored as a Neural Net Example</returns>
-        public List<Example> Play(Board board = null, Network network=null, bool logProgress=false)
+        public List<Example> Play(Board board = null, Network network = null, LogSetting setting = LogSetting.Ignore, GameViewer viewer = null)
         {
-            LogProgress = logProgress;
-            if (logProgress)
-                Logger.Show();
-
             if (board == null)
                 board = new Board();
             if (network == null)
                 network = new Network(board.Rows * board.Columns, 100, 1, null);
+            Viewer = viewer;
+            if (Viewer != null)
+                Log = Viewer.Log;
+            Log.Setting = setting;
 
             Bot allen = new Bot(Checker.Blue, network); // <-- you know he will win :)
             Bot jason = new Bot(Checker.Green, network);
 
             List<Example> trace = new List<Example>();
 
-            Turns=0;
+            Turns = 0;
             Bot current = allen;
-            while(!board.IsGameOver)
+            while (!board.IsGameOver)
             {
                 int column;
                 double score;
                 current.SelectMove(board, out column, out score);
-                Log((current == allen ? "Allen" : "Jason")
-			+ " picks column " + column
-			+ " (score " + score + ")");
+                Log.WriteLine(String.Format("{0} picks column {1}   (Score: {2:f2})", (current == allen ? "Allen" : "Jason"), column, score));
                 board.AddChecker(current.MyColor, column);
 
                 Example example = Transform.ToNormalizedExample(board, current.MyColor);
-		if (board.IsGameOver)
-		{
-			Checker winner1;
-            		if (board.TryGetWinner(out winner1))
-				score = winner1 == current.MyColor ? 1 : 0;
-			else
-				score = .5;
-		}
-                example.Labels.Add(score);
+                if (board.IsGameOver)
+                {
+                    Checker winner1;
+                    if (board.TryGetWinner(out winner1))
+                        score = winner1 == current.MyColor ? 1 : 0;
+                    else
+                        score = .5;
+                }
+                example.Predictions.Add(score);
                 trace.Add(example);
-                Log(ExampleToString(example));
-                
+
                 current = (current == allen ? jason : allen);
                 ++Turns;
             }
+
+            if (Viewer != null)
+                Viewer.BatchAddCheckers(board.MoveHistory, TimeSpan.FromMilliseconds(10));
+
             TotalTurns += Turns;
 
             Checker winner;
@@ -79,50 +71,27 @@ namespace ConnectFour
             {
                 if (winner == allen.MyColor)
                 {
-                    Log("WINNER:  Allen");
+                    Log.WriteLine("WINNER:  Allen");
                     ++AllenWon;
                 }
                 else
                 {
-                    Log("WINNER:  Jason");
+                    Log.WriteLine("WINNER:  Jason");
                     ++JasonWon;
                 }
             }
             else
             {
-                Log("TIE");
+                Log.WriteLine("TIE");
                 ++Ties;
             }
-            
+
             ++TotalGames;
-            Log(string.Format("Turns: {0} ({1:f2})", Turns, (double)TotalTurns / TotalGames));
-            Log(string.Format("Allen: {0}({1:f2}) Jason: {2}({3:f2}) Ties {4}({5:f2})   TOTAL: {6}", AllenWon, (double)AllenWon / TotalGames, JasonWon, (double)JasonWon / TotalGames, Ties, (double)Ties / TotalGames, TotalGames));
-            Log();
+            Log.WriteLine(string.Format("Turns: {0} ({1:f2})", Turns, (double)TotalTurns / TotalGames));
+            Log.WriteLine(string.Format("Allen: {0}({1:f2}) Jason: {2}({3:f2}) Ties {4}({5:f2})   TOTAL: {6}", AllenWon, (double)AllenWon / TotalGames, JasonWon, (double)JasonWon / TotalGames, Ties, (double)Ties / TotalGames, TotalGames));
+            Log.WriteLine();
 
             return trace;
         }
-
-        public void Log(string message="")
-        {
-            if (LogProgress)
-                Logger.WriteLine(message);
-        }
-
-	static String ExampleToString(Example example)
-	{
-		String buf = "";
-		for (int x = 0; x < 7; x++)
-		{
-			buf += "[";
-			for (int y = 0; y < 6; y++)
-			{
-				double d = example.Features[y * 7 + x];
-				buf += d > 0 ? "X" : d < 0 ? "O" : "-";
-			}
-			buf += "]";
-		}
-		buf += " : " + example.Labels[0];
-		return buf;
-	}
     }
 }
