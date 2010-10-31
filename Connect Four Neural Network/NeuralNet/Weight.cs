e8439d6e58e0559118fe7fad7aba88f738f04c1e
jason@long.name <jason@long.name@d487a409-c6bb-0f7a-8971-a662c59b5e07>
2010-10-31 19:17:18 +0000

    network: fix random values
    noticed that the generated weights were not very random,
    think this will fix it

--- Connect Four Neural Network/NeuralNet/Weight.cs
+++ Connect Four Neural Network/NeuralNet/Weight.cs
@@ -9,13 +9,18 @@ namespace NeuralNet
 	[Serializable]
 	public class Weight
 	{
+		// create a Random object once and use the same Random object
+		// each time a new random value is needed; otherwise you are
+		// not really random
+		static Random RANDOM = new Random();
+
 		public double Value;
 		public Weight(double value) { Value = value; }
 		public Weight(double min, double max)
 		{
 			min = Math.Min(min, max);
 			max = Math.Max(min, max);
-			Value = (new Random()).NextDouble() * (max - min) + min;
+			Value = RANDOM.NextDouble() * (max - min) + min;
 		}
 		public Weight(Tuple<double, double> range) : this(range.Item1, range.Item2) { }
 	}
