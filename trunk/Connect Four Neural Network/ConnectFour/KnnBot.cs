906f8831b1fe9a30928395c4fc25cd8ace5e99d4
jason@long.name <jason@long.name@d487a409-c6bb-0f7a-8971-a662c59b5e07>
2010-11-14 02:08:48 +0000

    k-nearest-neighbor: create a bot that uses k-nearest-neighbor

---
+++ trunk/Connect Four Neural Network/ConnectFour/KnnBot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using KNearestNeighbor;
+using NeuralNet;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ConnectFour
+{
+	/// <summary>
+	/// Plays the game of ConnectFour using a K-nearest-neighbor algorithm.
+	/// </summary>
+	public class KnnBot : Bot
+	{
+		KNearestNeighborLearner learner;
+
+		public KnnBot(Checker myColor) : base(myColor)
+		{
+			learner = new KNearestNeighborLearner(5, 1000);
+		}
+
+		protected override double EvaluateBoard(Board board)
+		{
+			Example example = Transform.ToNormalizedExample(board, MyColor);
+			learner.PropogateInput(example);
+			return example.Predictions[0];
+		}
+	}
+}
