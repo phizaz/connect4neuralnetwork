6f0b082b3cd6b9b33fee2d4c9fb9ef2a017bf11d
jason@long.name <jason@long.name@d487a409-c6bb-0f7a-8971-a662c59b5e07>
2010-11-22 02:12:06 +0000

    Bot: select move stochastically
    each time selecting a move, the Bot sets up a probability distribution,
    where the probability of selecting a move is based on how that move
    scores against other moves. The best moves get the highest probability,
    worse moves get smaller probabilities. The "Lambda" parameter can be
    used to control the probability. With a large lambda value (e.g. >100),
    the probabilities will be uniform. With small lambda value (must still
    be greater than 0, e.g. 0.01), then the probability will be very stilted
    toward the move with the greatest score.

--- trunk/Connect Four Neural Network/ConnectFour/Bot.cs
+++ trunk/Connect Four Neural Network/ConnectFour/Bot.cs
@@ -13,11 +13,14 @@ namespace ConnectFour
 	public abstract class Bot
 	{
 		public Checker MyColor;
+		public double Lambda;
+		static Random RANDOM = new Random();
 
 		protected Bot(Checker myColor)
 		{
             Debug.Assert(myColor != Checker.Empty);
 			MyColor = myColor;
+			Lambda = 0.01;
 		}
 
 		protected abstract double EvaluateBoard(Board board);
@@ -27,24 +30,62 @@ namespace ConnectFour
 
 		public void SelectMove(Board board, out int column, out double score)
 		{
-			int bestX = 0;
+			double[] columnEvaluations = new double[board.Columns];
 			double bestV = Double.NegativeInfinity;
 			for (int x = 0; x < board.Columns; x++)
 			{
 				if (!board.IsColumnFull(x))
 				{
-                    board.AddChecker(MyColor, x);
+					board.AddChecker(MyColor, x);
 					double v = EvaluateBoard(board);
 					board.RemoveChecker(x);
 					if (v > bestV)
 					{
-						bestX = x;
 						bestV = v;
 					}
+					columnEvaluations[x] = v;
+				}
+				else
+				{
+					columnEvaluations[x] = Double.NegativeInfinity;
 				}
 			}
-			column = bestX;
-            score = bestV;
+
+			//we pick a move randomly, using a probability distribution
+			//such that the moves with the "best" board positions have a
+			//higher probability of being selected
+
+			//higher values of Lambda mean choices will have more equal
+			//probability, even if they had different scores
+
+			//low values of Lambda will have the opposite effect
+			//Lambda should be positive number
+
+			Lambda = Math.Max(Lambda, 0.00001);
+			Lambda = Math.Min(Lambda, 10000);
+
+			double sum = 0.0;
+			double[] weights = new double[columnEvaluations.Length];
+			for (int i = 0; i < columnEvaluations.Length; i++)
+			{
+				// the closer this column's evaluation to the "best", the
+				// greater weight it will have
+				double w = 1 / (Lambda + (bestV - columnEvaluations[i]));
+				weights[i] = w;
+				sum += w;
+			}
+
+			double r = RANDOM.NextDouble() * sum;
+			int c;
+			for (c = 0; c + 1 < weights.Length; c++)
+			{
+				r -= weights[c];
+				if (r <= 0)
+					break;
+			}
+
+			column = c;
+			score = columnEvaluations[c];
 		}
 	}
 }
