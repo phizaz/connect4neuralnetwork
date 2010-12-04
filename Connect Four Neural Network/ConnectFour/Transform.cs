13fb488df9bb30a46f8290c1293725b6bdedee0c
jason@long.name <jason@long.name@d487a409-c6bb-0f7a-8971-a662c59b5e07>
2010-12-04 15:26:18 +0000

    replaceable transform: introduce 'Transform42' class
    With this change, you can now instantiate a 'Transform' object,
    and make a pointer to it. This way we can programmatically change
    which transform implementation will be in effect.
    
    Backwards compatibility is maintained, so if you call
    Transform.ToNormalizedExample (which is what our code in the rest of the
    program does), you'll still get the same thing you always did.
    
    Right now there is only one transform implementation: Transform42.

--- Connect Four Neural Network/ConnectFour/Transform.cs
+++ Connect Four Neural Network/ConnectFour/Transform.cs
@@ -8,20 +8,21 @@ using System.Diagnostics;
 namespace ConnectFour
 {
     /// <summary>
-    /// Contains the constant functions that transform Checkers/EndGameStates to double values to feed into neural network.
+    /// Contains the function that transform Connect-Four board states
+    /// into a vector of double values to feed into neural network.
     /// </summary>
-    public static class Transform
+    public abstract class Transform
     {
-        public static double ToValue(Checker checker) 
-        {
-            switch (checker)
-            {
-                case Checker.Blue: return -1.0; 
-                case Checker.Green: return 1.0; 
-                case Checker.Empty: return 0.0; 
-                default: throw new Exception();
-            }
-        }
+	static Transform myTransform = new Transform42();
+
+        /// <summary>
+        /// Converts board to neurel net training example.  Returns example corresponding to normalized board (current player's checker color corresponds to max value)
+        /// </summary>
+        /// <param name="lastPlayerToGo">Current Player which corresponds to last checker placed on board.</param>
+        public static Example ToNormalizedExample(Board board, Checker lastPlayerToGo)
+	{
+		return myTransform.MakeExample(board, lastPlayerToGo);
+	}
 
         public static double ToValue(GameResult result)
         {
@@ -30,6 +31,22 @@ namespace ConnectFour
                 case GameResult.Loss: return 0.1;
                 case GameResult.Win: return .9;
                 case GameResult.Draw: return 0.5;
+                default: throw new Exception();
+            }
+        }
+
+        public abstract Example MakeExample(Board board, Checker lastPlayerToGo);
+    }
+
+    public class Transform42 : Transform
+    {
+        public static double ToValue(Checker checker) 
+        {
+            switch (checker)
+            {
+                case Checker.Blue: return -1.0; 
+                case Checker.Green: return 1.0; 
+                case Checker.Empty: return 0.0; 
                 default: throw new Exception();
             }
         }
@@ -45,14 +62,10 @@ namespace ConnectFour
         }
 
 
-        /// <summary>
-        /// Converts board to neurel net training example.  Returns example corresopnding to normalized board (current player's checker color corresponds to max value)
-        /// </summary>
-        /// <param name="lastPlayerToGo">Current Player which corresponds to last checker placed on board.</param>
-        public static Example ToNormalizedExample(Board board, Checker lastPlayerToGo)
+        public override Example MakeExample(Board board, Checker lastPlayerToGo)
         {
             Debug.Assert(lastPlayerToGo != Checker.Empty);
-            return new Example(board.Cells.Cast<Checker>().Select(c=>Transform.ToNormalizedValue(c, lastPlayerToGo)).ToList());
+            return new Example(board.Cells.Cast<Checker>().Select(c=>Transform42.ToNormalizedValue(c, lastPlayerToGo)).ToList());
         }
   
     }
